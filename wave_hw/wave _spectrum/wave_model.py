30266c2a1b1f46562d00417a7c4a96600ddbc132
walter zesk <walter@conformlab.com>
2021-10-18 11:23:29 -0400

    added up crossings

--- wave_hw/wave _spectrum/wave_model.py
+++ wave_hw/wave _spectrum/wave_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-
 class wave_solver:
+    rho = 1025 #density
     grav = 9.80665  #m^2/s
     waveperiod = 0
     wavefrequency = 0
@@ -48,7 +48,18 @@ class wave_solver:
         #set dispersion relationship
         return dr                
 
-    
+    def amp_from_horizontal(self,horizonal_velocity,sampleheight):
+        #for spectral analysis, calculate amplitude based on horizontal velocity spectra
+
+        amp = horizonal_velocity / ( self.wavefrequency * (np.cosh(self.initial_wavenumber * (self.initial_depth + sampleheight)) / np.sinh(self.initial_wavenumber * self.initial_depth)))
+        return amp
+
+    def amp_from_vertical(self,vertical_velocity,sampleheight):
+        #for spectral analysis, calculate amplitude based on vertical velocity spectra
+
+        amp = vertical_velocity / (self.wavefrequency * (np.sinh(self.initial_wavenumber * (self.initial_depth + sampleheight)) / np.sinh(self.initial_wavenumber * self.initial_depth)))
+        return amp
+
     def horizontal_velocity(self,sampleheight):
         #horizontal velocity at a given depth -- data is averaged so leaving out final cos term
         u = self.initial_amplitude * self.wavefrequency * (np.cosh(self.initial_wavenumber * (self.initial_depth + sampleheight)) / np.sinh(self.initial_wavenumber * self.initial_depth))
@@ -62,19 +73,28 @@ class wave_solver:
     
     def wave_pressure(self,sampleheight):
         #pressure at a given depth -- data is averaged so leaving out final cos term
-        pr = (self.grav * self.initial_depth) + (self.initial_amplitude * self.grav * (np.cosh(self.initial_wavenumber * (self.initial_depth + sampleheight)) / np.cosh(self.initial_wavenumber * self.initial_depth)))
+
+        ##initial calculation
+        # pr = (self.grav * self.initial_depth) + (self.initial_amplitude * self.grav 
+        # * (np.cosh(self.initial_wavenumber * 
+        # (self.initial_depth + sampleheight)) / np.cosh(self.initial_wavenumber * self.initial_depth)))
+        
+        #based solution handed back in class
+        pr = (self.initial_amplitude * self.grav * self.rho) * (np.cosh(self.initial_wavenumber * 
+        (self.initial_depth + sampleheight)) / np.cosh(self.initial_wavenumber * self.initial_depth))
+
         return pr
 
-    def speed(self,wn,dr,eff_depth):
+    def speed(self,wn,eff_depth):
         #calculates wave phase speed, includes shallow water term
         wave_phase_speed = np.sqrt(self.grav / wn * np.tanh(wn * eff_depth))
         #print("phase speed: ",wave_phase_speed, " at depth: ",eff_depth)
         
         return wave_phase_speed
 
-    def group_speed(self,wn,dr,eff_depth):
+    def group_speed(self,wn,eff_depth):
         #calculates group speed, including shallow water term
-        wave_phase_speed = self.speed(wn,dr,eff_depth)
+        wave_phase_speed = self.speed(wn,eff_depth)
         wave_group_speed = wave_phase_speed * ( 0.5 + ( ( wn * eff_depth ) / np.sinh( 2 * wn * eff_depth ) ) )
         #print("group speed: ",wave_group_speed," at depth: ", eff_depth)
         
@@ -84,30 +104,72 @@ class wave_solver:
         #first updates wave number and carrier wave speed for shoal depth 
         #the calculate shoaling co, as the ratio of wave height at a given shoal depth to an initial deep water wave height.
         
-        deep_dr = self.dispersion(self.initial_depth)
-        initial_group_speed = self.group_speed(self.initial_wavenumber, deep_dr,self.initial_depth) #deep group speed
-        
+        initial_group_speed = self.group_speed(self.initial_wavenumber, self.initial_depth) #deep group speed
+
         shoal_dr = self.dispersion(shoal_depth) #dispersion relationship at shoal depth
         shoal_wave_number = shoal_dr * self.wavefrequency**2 / self.grav #wave number at shoal depth
-        shoal_group_speed = self.group_speed(shoal_wave_number, shoal_dr,shoal_depth) #shoal group speed
+        shoal_group_speed = self.group_speed(shoal_wave_number, shoal_depth) #shoal group speed
+
+        return np.sqrt(initial_group_speed / shoal_group_speed)#wave height ratio
         
-        return np.sqrt(shoal_group_speed / initial_group_speed) #wave height ratio
-        
-    def refraction_coefficient(self,incedent_angle,shoaling_co):
+    def refraction_coefficient(self,incedent_angle,shoal_depth):
         if incedent_angle == 0:
             return 1
-        
+
         #angle inputted in degrees, change to radians
         rad_incedent_angle = np.radians(incedent_angle)
-        
-        if -1 >= (np.sin(rad_incedent_angle) / shoaling_co) <= 1:#checking for acceptable input for arcsin
-            print("angle: ",incedent_angle," shoaling: ",shoaling_co," resulted in an arcsin input error")
+
+        #get initial phase speed and shoal phase speed -- C0 and C
+        initial_phase_speed = self.speed(self.initial_wavenumber, self.initial_depth) #deep group speed
+        shoal_dr = self.dispersion(shoal_depth) #dispersion relationship at shoal depth
+        shoal_wave_number = shoal_dr * self.wavefrequency**2 / self.grav #wave number at shoal depth
+        shoal_phase_speed = self.speed(shoal_wave_number, shoal_depth) #shoal group speed
+        phase_speed_ratio = shoal_phase_speed/initial_phase_speed # C/C0
+       
+        if -1 >= (np.sin(rad_incedent_angle) * phase_speed_ratio) >= 1:#checking for acceptable input for arcsin
+            print("angle: ",incedent_angle," shoaling: ",phase_speed_ratio," resulted in an arcsin input error")
             return 1
+       
+        #print("sin of incedent angle: ",np.sin(rad_incedent_angle),"group speed ratio: ",group_speed_ratio)
         
-        #calculate new refracted angle
-        rad_refracted_angle = np.arcsin(np.sin(rad_incedent_angle) / shoaling_co)
+        #calculate new refracted angle   I think this matches --> theta=arcsin(c*sin(theta_deep)/c_deep). 
+        rad_refracted_angle = np.arcsin(np.sin(rad_incedent_angle) * phase_speed_ratio)
         
         #new distance between rays
-        ray_span_ratio = np.cos(rad_refracted_angle)/np.cos(rad_incedent_angle)
+        ray_span_ratio =np.sqrt( np.cos(rad_refracted_angle)/np.cos(rad_incedent_angle))
         
         return ray_span_ratio
+
+def uu_to_pp(uxx,f,depth,sample_depth):
+    if f>0 and uxx>0 and f<4:
+        wave_period = 1/f
+        #print("calculated period: ",wave_period)
+        spec_wave = wave_solver(wave_period,depth,1)#starting with dummy amplitude of 1
+        spec_wave.initial_amplitude = spec_wave.amp_from_horizontal(uxx,sample_depth) # set a calculated amplitude
+        pressure = spec_wave.wave_pressure(sample_depth)
+        #print("calculated amp: ",spec_wave.initial_amplitude)
+        return pressure
+    elif():
+        print("f<0")
+        wave_period = 1
+        spec_wave = wave_solver(wave_period,depth,1)#starting with dummy amplitude of 1
+        spec_wave.initial_amplitude = spec_wave.amp_from_horizontal(1,sample_depth) # set a calculated amplitude
+        pressure = spec_wave.wave_pressure(sample_depth)
+        return pressure
+
+def ww_to_pp(wxx,f,depth,sample_depth):
+    if f>0 and wxx>0 and f<4:
+        wave_period = 1/f
+        #print("calculated period: ",wave_period)
+        spec_wave = wave_solver(wave_period,depth,1)#starting with dummy amplitude of 1
+        spec_wave.initial_amplitude = spec_wave.amp_from_vertical(wxx,sample_depth) # set a calculated amplitude
+        pressure = spec_wave.wave_pressure(sample_depth)
+        #print("calculated amp: ",spec_wave.initial_amplitude)
+        return pressure
+    elif():
+        print("f<0")
+        wave_period = 1
+        spec_wave = wave_solver(wave_period,depth,1)#starting with dummy amplitude of 1
+        spec_wave.initial_amplitude = spec_wave.amp_from_vertical(wxx,sample_depth) # set a calculated amplitude
+        pressure = spec_wave.wave_pressure(sample_depth)
+        return pressure
